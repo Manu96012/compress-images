29d74aefa0ef6cc83e18769956133a58f5dc84a6
Emanuele Filingeri <33067487+Manu96012@users.noreply.github.com>
2025-04-03 12:21:34 +0200

    Update compress_image_v2.sh
    Modifica in lingua inglese

--- compress_image_v2.sh
+++ compress_image_v2.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
 compress_and_save() {
-	# Immagine sorgente
+	# Source image
 	source_image="$1"
-	# Estensione di destinazione
+	# Destination extension
 	destination_image_extension="$2"
-	# Path assoluto dell'immagine sorgente
+	# Absolute path of the source image
 	source_image_absolute_path="$(realpath $1)"
-	# Estensione dell'immagine sorgente
+	# Extension of the source image
 	source_image_extension="${source_image##*.}"
-	# Filename dell'immagine sorgente
+	# Filename of the source image
 	source_image_filename="$(basename $source_image_absolute_path .$source_image_extension)"
-	# Path per la cartella contente l'immagine sorgente
+	# Path to the folder containing the source image
 	path_to_source_image_folder="$(realpath $(dirname $source_image_absolute_path))"
-	# Nuovo nome della folder delle immagini compresse
+	# New name for the compressed images folder
 	new_folder_name="compressed"
 
-				# Se non ho specificato un'estensione, lascio la medesima dell'immagine sorgente per la compressione
+				# If no extension is specified, keep the same as the source image for compression
 	if [ "$destination_image_extension" = "" ]; then
 		destination_image_extension=$source_image_extension
 	fi
@@ -25,37 +25,37 @@ compress_and_save() {
 		mkdir -p "${path_to_source_image_folder}/${new_folder_name}/${destination_image_extension}"
 	fi
 
-	# Eseguo la compressione in un'immagine con la seguente legenda: immagine_sorgente.png -> immagine_sorgente_compressed.<estensione desiderata>
+	# Compress the image with the following legend: source_image.png -> source_image_compressed.<desired_extension>
 	sips -s format $destination_image_extension -s formatOptions low $source_image_absolute_path --out "${path_to_source_image_folder}/${new_folder_name}/${destination_image_extension}/${source_image_filename}_compressed.${destination_image_extension}"
 }
 
 if [ $# -gt 0 ]; then
 	if [[ -f "$1" ]]; then
 
-		echo "Hai passato un file"
+		echo "You passed a file"
 		compress_and_save $1 $2
 
 	elif [[ -d "$1" ]]; then
 
-		echo "Hai passato una cartella"
+		echo "You passed a folder"
 		for _file in $1*; do
 
 			if [[ -f "$_file" ]]; then
 				compress_and_save $_file $2
 
 			else
-				echo "Skippando ${_file} in quanto non si tratta di file"
+				echo "Skipping ${_file} as it is not a file"
 
 			fi
 
 		done
 
 	else
-		echo "Ma che stai passando...?"
+		echo "What are you passing...?"
 
 	fi
 
 else
-	echo "Non è stato definito alcun parametro, specificare:\nImmagine da comprimere (mandatory)\nEstensione da sostituire (optional)"
+	echo "No parameters defined, please specify:\nImage to compress (mandatory)\nExtension to replace (optional)"
 	
 fi
